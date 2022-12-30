86599c97ce1a3976cea57ea58ad154757495030a
HansithaC <121653512+HansithaC@users.noreply.github.com>
2022-12-30 11:51:52 +0530

    Update Factorial_of_a_number.c

--- Factorial_of_a_number.c
+++ Factorial_of_a_number.c
@@ -4,23 +4,23 @@ int main()
 {
 	int n,f;
 	printf("Enter the num\n");     
-	scanf("%d",&n);         //taking the number as input
-	f=fact(n);      //calling the fuction
-	printf("Factorial %d",f);  //printing the factorial of the number
+	scanf("%d",&n);                                          //taking the number as input
+	f=fact(n);                                              //calling the fuction
+	printf("Factorial %d\n",f);                              //printing the factorial of the number
 }
 
 int fact(int n) 
 {
-	if(n==0)  //factorial of 0 is 1
+	if(n==0)                                                   //factorial of 0 is 1
 	{	
 		return 1;
 	}
-	else if(n==1)  //factorial of 1 is 1
+	else if(n==1)                                           //factorial of 1 is 1
 	{
 		return 1;
 	}
 	else
 	{
-		return n*fact(n-1); //for n other than 0 and 1
+		return n*fact(n-1);                             //for n other than 0 and 1
 	}
 }
