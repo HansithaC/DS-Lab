569cb544c0fd15a0477ba637d4e3054e3fba8ea6
HansithaC <121653512+HansithaC@users.noreply.github.com>
2022-12-30 11:24:15 +0530

    Update Binary_search.c

--- Binary_search.c
+++ Binary_search.c
@@ -5,11 +5,11 @@ int binarysearch(int arr[],int x,int low,int high)
 	{
 		int mid=low+(high-low)/2; 
 		if(arr[mid]==x)  
-			return mid;  //if element=arr[mid] element if found at mid
+			return mid;                                         //element=arr[mid] element if found at mid
 		else if(arr[mid]>x)  
-			return binarysearch(arr,x,low,mid-1);  // if element is less than mid value seacrh element from low to mid-1
+			return binarysearch(arr,x,low,mid-1);         // element is less than mid value seacrh element from low to mid-1
 		else 
-			return binarysearch(arr,x,mid+1,high); //else search element from mid+1 to high
+			return binarysearch(arr,x,mid+1,high);                   //else search element from mid+1 to high
 	}
 	return -1; 
 }
@@ -17,15 +17,15 @@ int binarysearch(int arr[],int x,int low,int high)
 int main(void)  
 
 {
-	int arr[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}; //initializing array 
-	int n=sizeof(arr)/sizeof(arr[0]); //to find size of the array
+	int arr[]={1,2,3,4,5,6,7,8,9,10,11,12,13,14,15};                          //initializing array 
+	int n=sizeof(arr)/sizeof(arr[0]);                                        //find size of the array
 	int x;
 	printf("Enter value between 1 to 15:");
-	scanf("%d",&x);         //reading x value
-	int result=binarysearch(arr,x,0,n-1); //low=0 high=n-1
+	scanf("%d",&x);                                                        //reading x value
+	int result=binarysearch(arr,x,0,n-1);                                 //low=0 high=n-1
 	if(result==-1)
-		printf("Value not found"); //if element is not found
+		printf("Value not found");                                //element is not found
 	else
-		printf("Required value is found at  %d",result); //print the index of the array where the element is found
+		printf("Required value is found at  %d",result);               //print the index of the array where the element is found
 
 }
