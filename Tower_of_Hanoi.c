08e3a39cb71fe580e91613be7b355bad90074948
HansithaC <121653512+HansithaC@users.noreply.github.com>
2022-12-30 12:04:31 +0530

    Update Tower_of_Hanoi.c

--- Tower_of_Hanoi.c
+++ Tower_of_Hanoi.c
@@ -1,12 +1,12 @@
 #include<stdio.h>
-void towers(int,char,char,char); //user defined function
+void towers(int,char,char,char);                                         //user defined function
 int main()  
 {
 	int num;
 	printf("Enter the no.of disks:"); 
-	scanf("%d",&num);               //intake the number of towers
-	printf("The size of moves involved\n");         //printing the number of moves the involved
-	towers(num,'A','C','B');  //function call 
+	scanf("%d",&num);                                          //taking the number of towers
+	printf("The size of moves involved\n");                     //printing the number of moves the involved
+	towers(num,'A','C','B');                                       //function call 
 	return 0;
 }
 
@@ -18,6 +18,6 @@ void towers(int num,char frompeg,char topeg,char auxpeg)
 		return;
 	}
 	towers(num-1,frompeg,auxpeg,topeg); //function call when num is not equal to 1
-	printf("\nmove disk %d from peg %c to prg %c",num,frompeg,topeg);
+	printf("\nmove disk %d from peg %c to peg %c",num,frompeg,topeg);
 	towers(num-1,auxpeg,topeg,frompeg);
 }
